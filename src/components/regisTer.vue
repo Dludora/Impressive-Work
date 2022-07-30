ed4ee0876c52a514984f3eae42eee51ccbaa8b72
growingmaster1 <861460595@qq.com>
2022-07-30 20:30:25 +0800

    ts setup

--- src/components/regisTer.vue
+++ src/components/regisTer.vue
@@ -50,7 +50,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .card-tabs .n-tabs-nav--bar-type {
   padding-left: 4px
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import regisTer from "@/components/regisTer"
-import homePage from "@/components/homePage";
+import regisTer from "../components/regisTer.vue";
+import homePage from "../components/homePage.vue";
 
 const routerHistory = createWebHistory()
 const router = createRouter({
     history: routerHistory,
     routes: [
         {
+            name:'homePage',
             path: '/',
             component: homePage
         },
         {
+            name:'regisTer',
             path: '/regisTer',
             component: regisTer
         },
