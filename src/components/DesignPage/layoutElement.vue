1ed5a52b6597beb70c640b1275075620621d407a
growingmaster1 <861460595@qq.com>
2022-08-02 13:55:28 +0800

    circle&text,deletable

--- src/components/DesignPage/layoutElement.vue
+++ src/components/DesignPage/layoutElement.vue
@@ -1,162 +1,311 @@
 <template>
-  <div :id="'content'+elementParams.id" class="content" @mouseenter="Highlight" @mouseleave="UnHighlight" @mousedown="select" @click.stop ref="content">
-    <slot name="content">
+  <div
+    :id="'content' + elementParams.id"
+    class="content"
+    @mouseenter="Highlight"
+    @mouseleave="UnHighlight"
+    @mousedown="select"
+    @click.stop="selectContent"
+    ref="content"
+    v-if="exist"
+  >
+    <div :id="'textDiv' + elementParams.id">
+      <n-input
+        :id="'textInputer' + elementParams.id"
+        ref="inputer"
+        v-if="textModifying"
+        v-model:value="text"
+        placeholder=""
+        type="textarea"
+        :autosize="{
+          minRows: 0,
+        }"
+      ></n-input>
+      <div v-show="!textModifying">{{ text }}</div>
+    </div>
 
-    </slot>
-    <div v-if="selected" id="upleft"  class="resizer cornerResizer upResizer leftResizer"></div>
-    <div v-if="selected" id="up" class="resizer upResizer tbCenterResizer"></div>
-    <div v-if="selected" id="upright" class="resizer cornerResizer upResizer rightResizer"></div>
-    <div v-if="selected" id="left" class="resizer leftResizer lrCenterResizer"></div>
-    <div v-if="selected" id="right" class="resizer rightResizer lrCenterResizer"></div>
-    <div v-if="selected" id="bottomleft" class="resizer cornerResizer bottomResizer leftResizer"></div>
-    <div v-if="selected" id="bottom" class="resizer bottomResizer tbCenterResizer"></div>
-    <div v-if="selected" id="bottomright" class="resizer cornerResizer bottomResizer rightResizer"></div>
+    <div
+      v-if="selected"
+      id="upleft"
+      class="resizer cornerResizer upResizer leftResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="up"
+      class="resizer upResizer tbCenterResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="upright"
+      class="resizer cornerResizer upResizer rightResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="left"
+      class="resizer leftResizer lrCenterResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="right"
+      class="resizer rightResizer lrCenterResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="bottomleft"
+      class="resizer cornerResizer bottomResizer leftResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="bottom"
+      class="resizer bottomResizer tbCenterResizer"
+    ></div>
+    <div
+      v-if="selected"
+      id="bottomright"
+      class="resizer cornerResizer bottomResizer rightResizer"
+    ></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref,watch } from "vue"
-import interact from "interactjs"
-import {gsap} from "gsap"
+import { onMounted, ref, watch } from "vue";
+import interact from "interactjs";
+import { gsap } from "gsap";
+import { InputInst } from "naive-ui";
 
 type Props = {
-  elementParams?:{
-    id:number,
-    x:number,
-    y:number,
-    height:number,
-    width:number,
-    typeOrSrc:string
-  }
-}
+  elementParams?: {
+    id: number;
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+    typeOrSrc: string;
+  };
+};
 
 const props = withDefaults(defineProps<Props>(), {
   elementParams: () => {
     return {
-      id:-1,
-      x:0,
-      y:0,
-      width:0,
-      height:0,
-      typeOrSrc:"Rect"
+      id: -1,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      typeOrSrc: "Rect",
     };
   },
 });
 
+const exist = ref<boolean>(true);
 const selected = ref<boolean>(false);
+const textModifying = ref<boolean>(false);
+const text = ref<string>("");
+const inputer = ref<InputInst | null>(null);
 
-const position = { x: 0, y: 0 }
+const snapGrid = interact.snappers.grid({
+  x: 10,
+  y: 10,
+});
+
+const position = { x: 0, y: 0 };
 
 interact(`#content${props.elementParams.id}`).draggable({
   listeners: {
-    start (event) {
-      console.log(event.type, event.target)
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
     },
-    move (event) {
-      position.x += event.dx
-      position.y += event.dy
+  },
+  modifiers: [
+    interact.modifiers.snap({
+      targets: [snapGrid],
+      range: Infinity,
+      relativePoints: [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+      ],
+    }),
+  ],
+});
 
-      event.target.style.transform =
-        `translate(${position.x}px, ${position.y}px)`
+interact(`#content${props.elementParams.id}`).resizable({
+  edges: { top: true, left: true, bottom: true, right: true },
+  modifiers: [
+    interact.modifiers.snap({
+      targets: [snapGrid],
+      range: Infinity,
+      relativePoints: [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+      ],
+    }),
+  ],
+  listeners: {
+    move: function (event) {
+      let { x, y } = position;
+
+      x = (x || 0) + event.deltaRect.left;
+      y = (y || 0) + event.deltaRect.top;
+
+      Object.assign(event.target.style, {
+        width: `${event.rect.width}px`,
+        height: `${event.rect.height}px`,
+        transform: `translate(${x}px, ${y}px)`,
+      });
+
+      Object.assign(position, { x, y });
     },
+  },
+});
+
+const Highlight = () => {
+  if (!selected.value && props.elementParams.typeOrSrc != "text") {
+    gsap.to(`#content${props.elementParams.id}`, {
+      duration: 0.15,
+      borderWidth: "4px",
+    });
   }
-})
+};
 
-interact(`#content${props.elementParams.id}`)
-  .resizable({
-    edges: { top: true, left: true, bottom: true, right: true },
-    listeners: {
-      move: function (event) {
-        let { x, y } = position
-
-        x = (x || 0) + event.deltaRect.left
-        y = (y || 0) + event.deltaRect.top
-
-        Object.assign(event.target.style, {
-          width: `${event.rect.width}px`,
-          height: `${event.rect.height}px`,
-          transform: `translate(${x}px, ${y}px)`
-        })
-
-        Object.assign(position, { x, y })
-      }
-    }
-  })
-
-const Highlight = ()=>{
-  if(!selected.value)
-  {
-    gsap.to(`#content${props.elementParams.id}`,{
-    duration:0.15,
-    borderWidth: "4px"
-  })
+const UnHighlight = () => {
+  if (!selected.value && props.elementParams.typeOrSrc != "text") {
+    gsap.to(`#content${props.elementParams.id}`, {
+      duration: 0.15,
+      borderWidth: "0px",
+    });
   }
-}
+};
 
-const UnHighlight = ()=>{
-  if(!selected.value)
-  {
-    gsap.to(`#content${props.elementParams.id}`,{
-    duration:0.15,
-    borderWidth: "0px"
-  })
-  }
-}
-
-const emits = defineEmits(["select"])
-const select = ()=>{
+const emits = defineEmits(["select", "destroy"]);
+let selectEns = false;
+const select = () => {
   console.log(props.elementParams.id);
   selected.value = true;
-  gsap.to(`#content${props.elementParams.id}`,{
-    duration:0.15,
-    borderWidth: "6px"
-  })
-  emits("select",props.elementParams.id);
-}
+  if (props.elementParams.typeOrSrc != "text") {
+    gsap.to(`#content${props.elementParams.id}`, {
+      duration: 0.15,
+      borderWidth: "6px",
+    });
+  }
 
-const UnSelect = ()=>{
+  selectEns = true;
+  emits("select", props.elementParams.id);
+  setTimeout(() => {
+    selectEns = false;
+  }, 500);
+};
+
+const selectContent = () => {
+  if (selectEns) {
+    switch (props.elementParams.typeOrSrc) {
+      case "text": {
+        textModifying.value = true;
+        break;
+      }
+    }
+  }
+};
+
+const UnSelect = () => {
   selected.value = false;
-  gsap.to(`#content${props.elementParams.id}`,{
-    duration:0.15,
-    borderWidth: "0px"
-  })
-}
+
+  switch (props.elementParams.typeOrSrc) {
+    case "text": {
+      console.log(text.value == "");
+      if (text.value == "") {
+        console.log(text.value);
+        emits("destroy", props.elementParams.id);
+      }
+      textModifying.value = false;
+      break;
+    }
+    default: {
+      gsap.to(`#content${props.elementParams.id}`, {
+        duration: 0.15,
+        borderWidth: "0px",
+      });
+    }
+  }
+};
 
 defineExpose({
   UnSelect,
-})
+});
 
-onMounted(()=>{
+onMounted(() => {
   console.log("mounted");
-  console.log(document.getElementById("up"));
-  document.getElementById(`content${props.elementParams.id}`)!.style.transform=`translate(${props.elementParams.x}px, ${props.elementParams.y}px)`;
+  console.log(inputer.value);
+  document.getElementById(
+    `content${props.elementParams.id}`
+  )!.style.transform = `translate(${props.elementParams.x}px, ${props.elementParams.y}px)`;
   position.x = props.elementParams.x;
   position.y = props.elementParams.y;
-  document.getElementById(`content${props.elementParams.id}`)!.style.width = `${props.elementParams.width}px`;
-  document.getElementById(`content${props.elementParams.id}`)!.style.height = `${props.elementParams.height}px`;
-  switch(props.elementParams.typeOrSrc)
-  {
-    case "circle":{
-      document.getElementById(`content${props.elementParams.id}`)!.style.borderRadius = "50% 50%";
+  document.getElementById(
+    `content${props.elementParams.id}`
+  )!.style.width = `${props.elementParams.width}px`;
+  document.getElementById(
+    `content${props.elementParams.id}`
+  )!.style.height = `${props.elementParams.height}px`;
+  switch (props.elementParams.typeOrSrc) {
+    case "circle": {
+      document.getElementById(
+        `content${props.elementParams.id}`
+      )!.style.borderRadius = "50% 50%";
+      break;
     }
-    case "text":{
-      
+    case "text": {
+      Object.assign(
+        document.getElementById(`content${props.elementParams.id}`)!.style,
+        {
+          width: `auto`,
+          height: `auto`,
+          backgroundColor: `transparent`,
+          borderColor: `transparent`,
+        }
+      );
+      document.getElementById(
+        `textDiv${props.elementParams.id}`
+      )!.style.padding = "20px";
+      textModifying.value = true;
     }
   }
-})
+  select();
+});
 
-watch(()=>props.elementParams,(newVal)=>{
-  console.log(document.getElementById("up"));
-  document.getElementById(`content${newVal.id}`)!.style.transform=`translate(${newVal.x}px, ${newVal.y}px)`;
-  position.x = newVal.x;
-  position.y = newVal.y;
-  document.getElementById(`content${newVal.id}`)!.style.width = `${newVal.width}px`;
-  document.getElementById(`content${newVal.id}`)!.style.height = `${newVal.height}px`
-})
+watch(
+  () => props.elementParams,
+  (newVal) => {
+    if (newVal == null) {
+      exist.value = false;
+      return;
+    }
+    if (document.getElementById(`content${newVal.id}`) == null) {
+      return;
+    }
+    document.getElementById(
+      `content${newVal.id}`
+    )!.style.transform = `translate(${newVal.x}px, ${newVal.y}px)`;
+    position.x = newVal.x;
+    position.y = newVal.y;
+    document.getElementById(
+      `content${newVal.id}`
+    )!.style.width = `${newVal.width}px`;
+    document.getElementById(
+      `content${newVal.id}`
+    )!.style.height = `${newVal.height}px`;
+  },
+  {
+    deep: true,
+    immediate: true,
+  }
+);
 </script>
 
 <style scoped>
-.content{
+.content {
   display: block;
   position: absolute;
   background-color: red;
@@ -167,7 +316,7 @@ watch(()=>props.elementParams,(newVal)=>{
   height: 200px;
   box-sizing: border-box;
 }
-.resizer{
+.resizer {
   display: block;
   position: absolute;
   z-index: 10000;
@@ -178,29 +327,29 @@ watch(()=>props.elementParams,(newVal)=>{
   width: 6px;
   height: 6px;
 }
-.leftResizer{
-  left:-8px;
+.leftResizer {
+  left: -8px;
 }
-.rightResizer{
-  right:-8px;
+.rightResizer {
+  right: -8px;
 }
-.upResizer{
-  top:-8px;
+.upResizer {
+  top: -8px;
 }
-.bottomResizer{
-  bottom:-8px
+.bottomResizer {
+  bottom: -8px;
 }
-.tbCenterResizer{
+.tbCenterResizer {
   margin: auto;
   left: 0;
-  right:0;
+  right: 0;
 }
-.lrCenterResizer{
+.lrCenterResizer {
   top: 0;
-  bottom:0;
+  bottom: 0;
   margin: auto;
 }
-.cornerBlock{
+.cornerBlock {
   position: absolute;
   text-align: center;
 }
