5a11e7a049386ceebd63b9e68ba4cb6966771bb3
聂湛恒 <nzh20010713@163.com>
2022-08-04 21:38:59 +0800

    adding-uml

---
+++ src/components/UML.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="big-contain">
+    <n-config-provider  :theme="darkTheme">
+      <div>
+        <n-gradient-text type="info">
+          Live Forever
+        </n-gradient-text>
+      </div>
+    </n-config-provider>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style scoped>
+.big-contain{
+  width:fit-content;
+  margin:39px 43px 0 61px;
+}
+
+.n-gradient-text {
+  font-size: 36px;
+}
+</style>
