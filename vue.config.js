7a1909e04eb4977b13e3526d79dbef03a43c20ed
聂湛恒 <nzh20010713@163.com>
2022-08-08 08:59:45 +0800

    change

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
     configureWebpack: {
         resolve: {
-            fallback: { path: require.resolve('path-browserify'),
-                        assert: require.resolve("assert/"),
-                        crypto: require.resolve("crypto-browserify"),
-                        os: require.resolve("os-browserify/browser"),
-                        stream: require.resolve("stream-browserify"),
-                        util: require.resolve("util/"),
-                        fs: require.resolve('fs'),
-                        child_process: require.resolve("child_process"),
-                        rimraf: require.resolve("rimraf"),
+            fallback: {
             }
         }
     }
